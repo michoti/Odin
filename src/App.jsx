dbeb1a4b92962bd8148301f31b8f8ae64bf009f6
shukeel <triggaillest@gmail.com>
2022-07-21 22:38:35 +0300

    modified

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { About } from './components/about/About'
+import { Contact } from './components/contact/Contact'
+
+export const App = () => {
+  return (
+    <>
+    <About />
+    <Contact/>
+    </>
+  )
+}
