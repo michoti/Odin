63ea45673334290fc7eb92186d91ca9345321a6d
shukeel <triggaillest@gmail.com>
2022-07-22 11:21:55 +0300

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { About } from './components/about/About'
 import { Contact } from './components/contact/Contact'
+import Header from './components/Header/Header'
 
 export const App = () => {
   return (
     <>
+    <Header />
     <About />
     <Contact/>
     </>
